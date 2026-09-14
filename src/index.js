fe987c49d828dc60809bd979cd420ae85ab67653
Hukla <129692708+huklaa@users.noreply.github.com>
2026-09-14 17:27:12 +0300

    feat: add src/index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export {
+  ARC_TESTNET,
+  ARC_USDC_SYSTEM_EMITTER,
+  formatNativeUsdc,
+  transactionFee,
+} from "./arc.js";
+export {
+  buildEvidenceEnvelope,
+  fetchAllPages,
+  mergeEvidenceEnvelopes,
+} from "./coverage.js";
+export { summarizeUsdcActivity } from "./activity.js";
